8100d05d4b1305856be6fa5005eaf916fbf6ac80
doctype-Melvin <doctype.melvin@gmail.com>
2023-05-10 08:36:32 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,7 @@ export default function Home() {
   return (
     <>
       <main >
-          {/* <div>
-            <Link href="/exercises">Go to exercises</Link>
-          </div>
-          <div>
-            <Link href="/plans">Go to plans</Link>
-          </div> */}
-          <div>
-            <Link href="/create">Create plan</Link>
-          </div>
+          
       </main>
     </>
   )
