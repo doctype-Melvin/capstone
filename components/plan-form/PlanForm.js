6b3b6d18cf78476cd7dc9060047db8dccc27104b
doctype-Melvin <doctype.melvin@gmail.com>
2023-05-10 16:17:09 +0200

    adds just what US1 says

--- components/plan-form/PlanForm.js
+++ components/plan-form/PlanForm.js
@@ -2,36 +2,25 @@ import Link from "next/link"
 import { useState } from "react"
 import { GlobalContext } from "@/components/GlobalProvider";
 import { useContext } from "react";
-import WorkoutDay from "../workout-day/WorkoutDay";
-import { nanoid } from "nanoid";
 import { addWorkoutDays } from "@/utils/helpers";
 
 export default function PlanForm() {
     const [planData, updatePlanData] = useContext(GlobalContext)
     
-    const [ numberOfDays, setNumberOfDays ] = useState(0)
-
-
-
     const handleSave = (event) => {
         event.preventDefault()
 
-        const formData = new FormData(event.target)
-        const nameAndDays = Object.fromEntries(formData)
-        const routineArray = addWorkoutDays(numberOfDays)
+        // const formData = new FormData(event.target)
+        // const nameAndDays = Object.fromEntries(formData)
+        // const routineArray = addWorkoutDays(Number(nameAndDays.days))
         
-        updatePlanData(draft => {
-            draft.name = nameAndDays.name
-            draft.days = nameAndDays.days
-            draft.routine = routineArray
-        })
+        // updatePlanData(draft => {
+        //     draft.name = nameAndDays.name
+        //     draft.days = nameAndDays.days
+        //     draft.routine = routineArray
+        // })
 
-        console.log(nameAndDays, planData, routineArray)
-    }
 
-    const handleChange = (event) => {
-        setNumberOfDays(Number(event.target.value))
-        
     }
 
     return (
@@ -41,8 +30,8 @@ export default function PlanForm() {
                 <input 
                 type="text" 
                 name="name" 
-                min={2}
-                max={20}
+                minLength={2}
+                maxLength={20}
                 required />
                 <label htmlFor="days">Days</label>
                 <input 
@@ -50,13 +39,7 @@ export default function PlanForm() {
                 name="days"
                 min={1}
                 max={7}
-                onChange={(event) => handleChange(event)} 
                 required />
-                {/* { 
-                numberOfDays ? (
-                    Array.from(Array(numberOfDays).keys()).map(day => <WorkoutDay key={day} number={day+1} />)
-                ) : null
-                } */}
                 <button type="submit">Save</button>
             </form>
             <Link href="/">Back</Link>
