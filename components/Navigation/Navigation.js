d7f0cf7f97c5dcd196dbc1f71f043c9841bdec78
doctype-Melvin <doctype.melvin@gmail.com>
2023-05-23 10:50:11 +0200

    adds icon library and icons to navbar

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,20 +1,38 @@
 import Link from "next/link"
 import styled from "styled-components"
+import { BiHomeAlt2 as Home }  from "react-icons/bi"
+import { AiOutlineUnorderedList as AllTemplates } from "react-icons/ai"
 
 const NavBar = styled.nav`
 width: 100%;
 display: flex;
 justify-content: space-around;
+align-items: center;
 position: absolute;
 bottom: 0;
+background-color: hotpink;
+`
+
+const NavIconContainer = styled.section`
+font-size: 1.6rem;
+color: white;
+padding: 5px 0;
 `
 
 export default function Navigation() {
 
     return (
         <NavBar>
-            <Link href="/">Home</Link>
-            <Link href="/viewPlans">All Templates</Link>
+            <Link href="/">
+                <NavIconContainer>
+                <Home />
+                </NavIconContainer>
+            </Link>
+            <Link href="/viewPlans">
+                <NavIconContainer>
+                <AllTemplates/>
+                </NavIconContainer>
+            </Link>
         </NavBar>
     )
 }
