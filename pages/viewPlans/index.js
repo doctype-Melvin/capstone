01836ceede39b342d8d16d389c478529a7acd71a
doctype-Melvin <doctype.melvin@gmail.com>
2023-05-23 12:51:05 +0200

    adds TemplateCard to ViewAllPlans page rendering

--- pages/viewPlans/index.js
+++ pages/viewPlans/index.js
@@ -1,6 +1,7 @@
 import { useAllPlans } from "@/utils/helpers";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import TemplateCard from "@/components/TemplateCard/TemplateCard";
 
 export default function ViewAllPlans() {
   const router = useRouter()
@@ -11,7 +12,7 @@ export default function ViewAllPlans() {
   return (
     <div>
       <p>{`There are`} {data.length} {data.length === 1 ? 'template' : 'templates'} {`in your vault`}</p>
-      {data.map(plan => <div key={plan._id}><Link href={`viewPlans/${plan._id}`}>{plan.name}</Link></div>)}
+      {data.map(plan => <div key={plan._id}><Link href={`viewPlans/${plan._id}`}><TemplateCard data={plan} /></Link></div>)}
       <button type="button" onClick={() => router.push(`/createPlan`)}>New Template</button>
     </div>
   );
