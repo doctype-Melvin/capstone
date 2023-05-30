f9a65afb45b7b468cd9075095c4a4da60e618e8e
doctype-Melvin <doctype.melvin@gmail.com>
2023-05-30 12:23:52 +0200

    adds min-height to index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import styled, { css } from "styled-components";
 
 const HomeScreen = styled.section`
-  height: calc(100vh - var(--navbar-height));
+  min-height: 100vh;
   display: grid;
   grid-template-rows: repeat(3, 1fr);
   grid-template-columns: 1fr;
