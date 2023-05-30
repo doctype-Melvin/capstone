d27c5de81950fc108358153dc4d51fbeca33ef6f
doctype-Melvin <doctype.melvin@gmail.com>
2023-05-30 11:44:47 +0200

    changes layout height property to min-height

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,7 +2,7 @@ import Navigation from "../Navigation";
 import styled from "styled-components";
 
 const LayoutContainer = styled.section`
-  height: 100vh;
+  min-height: 100vh;
 `;
 
 export default function Layout({ children }) {
