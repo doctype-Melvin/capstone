dc3e0d4a30a28f6019300b3fde78e4fec8a80c36
Melvin Speckamp <doctype.melvin@gmail.com>
2023-06-04 11:39:53 +0200

    adds history icon to nav and basic history rendering

--- pages/history/[id].js
+++ pages/history/[id].js
@@ -2,6 +2,7 @@ import Loading from "@/components/Loading";
 import { usePlan } from "@/utils/helpers";
 import { useRouter } from "next/router";
 
+
 export default function HistoryPage() {
     const router = useRouter()
     const { id } = router.query
@@ -10,8 +11,37 @@ export default function HistoryPage() {
     if (isLoading || !sessionHistory) return <Loading />
 
     return (
-        <>
-        Render list here
-        </>
+        <section>
+            <ul>
+                {
+                    sessionHistory.sessions.map(week => (
+                        <li key={week.week}>
+                            <div>Week {week.week}</div>
+                            <ul>
+                                {
+                                    week.sessions.map(session => (
+                                        <li key={session.id}>
+                                            <span>{session.sessionDate}</span>
+                                            <ul>
+                                                {
+                                                    session.result.map((set, index) => (
+                                                        <li key={set.setId}>
+                                                            <p>{set.exercise}</p>
+                                                            <span>Set {index+1}</span>
+                                                            <span>Reps {set.reps}</span>
+                                                            <span>Weight {set.weight}</span>
+                                                        </li>
+                                                    ))
+                                                }    
+                                            </ul>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </li>
+                    ))
+                }
+            </ul>
+        </section>
     )
 }
